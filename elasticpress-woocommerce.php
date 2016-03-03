d2284e4bb61de92c3a69ca858e3ec0a0073fca95
Taylor Lovett <tlovett88@gmail.com>
2016-03-03 01:19:01 -0500

    Remake post statuses array to fix key issues; increment version

--- elasticpress-woocommerce.php
+++ elasticpress-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ElasticPress WooCommerce
  * Description: Integrate ElasticPress and Elasticsearch with WooCommerce
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Taylor Lovett, 10up
  * Author URI:  http://10up.com
  * License:     GPLv2 or later
@@ -427,6 +427,8 @@ function epwc_formatted_args( $formatted_args, $args ) {
 			if ( $trash_index ) {
 				unset( $post_status[ $trash_index ] );
 			}
+
+			$post_status = array_values( $post_status );
 		}
 	} else {
 
